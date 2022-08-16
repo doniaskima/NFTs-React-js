3ee033e845bc4a538c0c65701b4465006f8a7ad1
doniaskima <doniaskima344@gmail.com>
2022-08-16 15:22:05 +0100

    add NFTs

--- src/components/TopHero/TopHero.js
+++ src/components/TopHero/TopHero.js
@@ -35,6 +35,38 @@ const TopLeft = () => {
                   </div>
               </div>
           </div>
+          <div className="top-right">
+              <div className="top-right-nfts">
+                  <div className="top-right-nfts-item">
+                      <img
+                          src="https://openseauserdata.com/files/22284fa002c7612a875381ab66b22abf.gif"
+                          className="tf-r-diamond-img"
+                          alt="diamond banner"
+                      />
+                  </div>
+                  <div className="top-right-nfts-item">
+                      <img
+                          src="https://openseauserdata.com/files/c3a312c53514642e8041c65e10b40a52.gif"
+                          className="tf-r-diamond-img"
+                          alt="diamond banner"
+                      />
+                  </div>
+                  <div className="top-right-nfts-item">
+                      <img
+                          src="https://openseauserdata.com/files/0895e82346f5c31462d41ab75d9b90b1.gif"
+                          className="tf-r-diamond-img"
+                          alt="diamond banner"
+                      />
+                  </div>
+                  <div className="top-right-nfts-item">
+                      <img
+                          src="https://openseauserdata.com/files/24ed399dc3e5430b39a1db656f68c67c.gif"
+                          className="tf-r-diamond-img"
+                          alt="diamond banner"
+                      />
+                  </div>
+              </div>
+          </div>
     </div>
   )
 }
